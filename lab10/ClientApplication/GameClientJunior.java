6a42d365b824dcb55af472bff989329c3bd7bd6c
teodoratrinca <61377986+teodoratrinca@users.noreply.github.com>
2020-05-03 13:07:48 +0300

    Create GameClientJunior.java

---
+++ lab10/ClientApplication/GameClientJunior.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class GameClientJunior {
+    static int port=2727;
+   Socket socket;
+    String serverAddress = "127.0.0.1";
+
+    public GameClientJunior() throws IOException {
+        this.socket = new Socket(serverAddress, port);
+    }
+    public String read(){
+       Scanner scanner=new Scanner(System.in);
+       return  scanner.next();
+    }
+    public void send(String request)throws IOException{
+        OutputStreamWriter out=new OutputStreamWriter(socket.getOutputStream());
+        BufferedWriter output=new BufferedWriter(out);
+    }
+
+    public static void main(String[] args) {
+       try{
+           GameClientJunior gameClientJunior=new GameClientJunior();
+           while(true)
+           {
+               //Scanner scanner=new Scanner(System.in);
+               String request=gameClientJunior.read();
+               gameClientJunior.send(request);
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
+}
+
+
