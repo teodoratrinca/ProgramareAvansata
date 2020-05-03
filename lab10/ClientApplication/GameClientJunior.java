b45866fbe719d4bef7ca0e4ca4f5465e783e78f1
teodoratrinca <61377986+teodoratrinca@users.noreply.github.com>
2020-05-03 13:14:53 +0300

    Update GameClientJunior.java

--- lab10/ClientApplication/GameClientJunior.java
+++ lab10/ClientApplication/GameClientJunior.java
@@ -10,15 +10,15 @@ public class GameClientJunior {
     public GameClientJunior() throws IOException {
         this.socket = new Socket(serverAddress, port);
     }
-    public String read(){
-       Scanner scanner=new Scanner(System.in);
-       return  scanner.next();
-    }
     public void send(String request)throws IOException{
         OutputStreamWriter out=new OutputStreamWriter(socket.getOutputStream());
         BufferedWriter output=new BufferedWriter(out);
     }
-
+    public String read(){
+       Scanner scanner=new Scanner(System.in);
+       return  scanner.next();
+    }
+    
     public static void main(String[] args) {
        try{
            GameClientJunior gameClientJunior=new GameClientJunior();
